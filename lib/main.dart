f1a7943a9ab08fd1adf63130a361dd87367f1f6e
manav <globalgroup850@gmail.com>
2022-08-16 22:15:06 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import './quiz.dart';
+import './result.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What is your favorite Color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite Animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 8},
+        {'text': 'Elephant', 'score': 7},
+        {'text': 'Lion', 'score': 10},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite Country?',
+      'answers': [
+        {'text': 'India', 'score': 10},
+        {'text': 'China', 'score': 3},
+        {'text': 'Russia', 'score': 8},
+        {'text': 'America', 'score': 5},
+      ],
+    },
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('My First App')),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions)
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
+
+class Quiz extends StatelessWidget {
+  final List<Map<String, Object>> questions;
+  final int questionIndex;
+  final ValueSetter<int> answerQuestion;
+
+  Quiz({
+    required this.questions,
+    required this.answerQuestion,
+    required this.questionIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Question(
+          questions[questionIndex]['questionText'] as String,
+        ),
+        ...(questions[questionIndex]['answers'] as List<Map<String, dynamic>>)
+            .map((answer) {
+          return Answer(
+              () => answerQuestion(int.parse(answer['score'].toString())),
+              answer['text']);
+        }).toList()
+      ],
+    );
+  }
+}
